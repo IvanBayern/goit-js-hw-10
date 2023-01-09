17c369c2a318d0f50edc2d59d992477b2c3be81a
Ivan <96693815+IvanBayern@users.noreply.github.com>
2023-01-09 19:57:04 +0100

    add hw10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 import './css/styles.css';
 
+
+
+// const response = fetch('https://restcountries.com/v2/name/peru');
+
+// response.then(resp => resp.json()).then(data => console.log(data))
+
+const inputRef = document.querySelector("#search-box");
+const countryListRef = document.querySelector('.country-list');
+const countryInfoRef = document.querySelector('.country-info');
+
 const DEBOUNCE_DELAY = 300;
+const MAX_COUNTRIES = 10;
+const MORE_MAX = 'More MAX';
+
+
+const BASE_URL = ''
+
+
+// input.addEventListener('submit', fetchCountries)
+
+// function fetchCountries(name) {
+//     name.preventDefault();
+//     const { query } = name.currentTarget;
+//    fetchCountriesApi(countri.value).then()
+// }
+
+// function fetchCountriesApi(countri) {
+//     const BASE_URL = 'https://restcountries.com/v2/name/peru'
+//     // const key = ''
+//     return fetch(`${BASE_URL}/q=${countri} `)
+// }
